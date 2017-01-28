5baeea83b073a5c0fc4b9447c6d1354c8398294e
jrdn hannah <jrdn@jrdnhannah.co.uk>
2017-01-28 16:56:08 +0000

    [Users] Role and hospital name

---
+++ src/User/UserRole.php
@@ -0,0 +1,14 @@
+<?php declare(strict_types=1);
+
+namespace NhsHd\AccidentalCt\User;
+
+/**
+ * Class UserRole
+ *
+ * @author jrdn hannah <jrdn@jrdnhannah.co.uk>
+ */
+final class UserRole
+{
+    const ROLE_ADMINISTRATOR = 1;
+    const ROLE_RADIOGRAPHER  = 2;
+}
