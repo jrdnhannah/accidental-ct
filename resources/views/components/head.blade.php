44d237eeb032ccf723c75fd3d3d342c4e91a1694
jrdn hannah <jrdn@jrdnhannah.co.uk>
2017-01-29 10:58:21 +0000

    Add missing CSS file

--- resources/views/components/head.blade.php
+++ resources/views/components/head.blade.php
@@ -1,4 +1,5 @@
 <head>
     <link rel="stylesheet" type="text/css" href="/assets/css/accidental-ct.css" />
+    <link rel="stylesheet" type="text/css" href="/assets/css/utilities.css"/>
     <title>{{ $title ?? 'CT Scan' }}</title>
 </head>
