cc956900ccc9827829a0c65ef7efd83f4a4bb027
jrdn hannah <jrdn@jrdnhannah.co.uk>
2017-01-29 11:44:38 +0000

    Registration/login fixes

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,29 +2,22 @@
 
 @section('content')
     <div id="login-wrapper" class="section-wrapper">
-        <div id="login-form">
-            <div id="login-real-name-wrapper">
-                <label class="form-label">Real name<input type="text" id="login-real-name-text" /></label>
+        <form class="form" method="post" action="{{ route('auth.post_login') }}">
+            <div id="login-form">
+                <div id="login-real-name-wrapper">
+                    <label class="form-label">Email<input type="text" name="email" id="login-real-name-text" /></label>
+                </div>
+                <div id="login-password-wrapper">
+                    <label class="form-label">Password<input type="password" name="password" id="login-password-text" /></label>
+                </div>
+                <div id="register-or-forgot-password">
+                    <span id="register">Register</span>
+                    &nbsp;|&nbsp;
+                    <span id="forgot-your-password">Forgot your password?</span>
+                </div>
+                {{ csrf_field() }}
+                <button type="submit">Log In</button>
             </div>
-            <div id="login-password-wrapper">
-                <label class="form-label">Password<input type="text" id="login-password-text" /></label>
-            </div>
-            <div id="register-or-forgot-password">
-                <span id="register">Register</span>
-                &nbsp;|&nbsp;
-                <span id="forgot-your-password">Forgot your password?</span>
-            </div>
-        </div>
-    </div>
-    <div id="register-form" class="section-wrapper disappear">
-        <div id="register-real-name-wrapper">
-            <label class="form-label">Real name<input type="text" id="register-real-name-text" /></label>
-        </div>
-        <div id="register-email-address-wrapper">
-            <label class="form-label">Email address<input type="text" id="register-email-address-text" /></label>
-        </div>
-        <div id="register-password-wrapper">
-            <label class="form-label">Password<input type="text" id="register-password-text" /></label>
-        </div>
+        </form>
     </div>
 @stop
