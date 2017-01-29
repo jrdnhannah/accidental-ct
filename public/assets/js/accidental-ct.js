d0a5511f53f7b5c37e19702f2afecc49145a3cd0
jrdn hannah <jrdn@jrdnhannah.co.uk>
2017-01-29 12:28:26 +0000

    fix js

--- public/assets/js/accidental-ct.js
+++ public/assets/js/accidental-ct.js
@@ -1,19 +1,4 @@
 $(document).ready(function(){
-    $('#login-or-register-text').click(function(){
-        $('#login-wrapper').removeClass('disappear');
-        $('#main-wrapper').addClass('disappear');
-    });
-
-    $('#forgot-your-password-text').click(function(){
-        $('#login-wrapper').addClass('disappear');
-        $('#register-wrapper').removeClass('disappear');
-    });
-
-    $('#register-text').click(function(){
-        $('#login-wrapper').removeClass('disappear');
-        $('#register-wrapper').removeClass('disappear');
-    });
-
     $('input, textarea').on('keydown keyup change click', function(){
         var identifier = $(this).attr('name');
         var content = $(this).val();
