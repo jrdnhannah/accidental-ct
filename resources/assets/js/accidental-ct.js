eb01f92e43306f33bc505029451dcd636e86b92c
jrdn hannah <jrdn@jrdnhannah.co.uk>
2017-01-29 10:31:52 +0000

    Assets (CSS/JS)

--- resources/assets/js/accidental-ct.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function(){
-    $('#register').click(function(){
-        $('#login-form').toggleClass('disappear');
-        $('#register-form').toggleClass('disappear');
-    });
-});
